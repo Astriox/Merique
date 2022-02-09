ea131a0016668c613c65d8d55c51ac3288307ecd
Joel Boban <90124641+JoelBoban@users.noreply.github.com>
2022-02-09 15:49:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,6 +48,7 @@ LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False")
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
+NO_MORE_PRINTS = environ.get('NOT_RELEASED')
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
