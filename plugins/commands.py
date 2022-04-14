e5f604daf7809fe19de3523e94785a29896fe9c9
Jσєℓ Bσвαη [Oғғlιɴe] <95572329+JoelBobanOffline@users.noreply.github.com>
2022-04-14 07:46:55 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -110,10 +110,14 @@ async def start(client, message):
             f_caption=f_caption
     if f_caption is None:
         f_caption = f"{files.file_name}"
+    buttons = [[
+        InlineKeyboardButton(text="Join Here", url="https://t.me/joel_noob")
+    ]]
     await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup(buttons)
         )
                     
 
